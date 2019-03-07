b3af5afd5f4938e3d4a0894c4c0f61f60649dfb1
lawrence910426 <lawrence910426@gmail.com>
2019-03-07 09:04:45 +0800

    Fei Yu GGYY
    Sorry sean i deleted your file

--- Applications/backend/backend_proc/config.php
+++ Applications/backend/backend_proc/config.php
@@ -4,23 +4,23 @@ function config()
 {
     return [
         "bank" => [
-            "ip" => "192.168.0.150",
+            "ip" => "192.168.56.1",
             "port" => 8787,
             "password" => "Fei Yu GGYY"
         ],
         "database" => [
             "ip" => "localhost",
-            "account" => "root",
-            "password" => "",
+            "account" => "dinnersystem",
+            "password" => "2rjurrru",
             "name" => "dinnersys"
         ],
         "login" => [
-            "time" => "60",
+            "time" => "30",
             "tolerance" => "3",
             "punish" => "60"
         ],
         "payment" => [
-            "time" => "60",
+            "time" => "30",
             "tolerance" => "3",
             "punish" => "60"
         ]
