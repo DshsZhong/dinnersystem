6c0c5fbba20d9d854dbd07a73355d5c0ecbf1da8
lawrence910426 <lawrence910426@gmail.com>
2019-03-18 23:44:43 +0800

    Factory cleint UI fix

--- Attach/bank_server/bank_server/Main_Controller.cs
+++ Attach/bank_server/bank_server/Main_Controller.cs
@@ -144,9 +144,7 @@ namespace bank_server
                 }
                 row[0] = "寫入";
                 row[1] = json.uid.ToString();
-                row[1] = json.uid.ToString();
-                row[1] = json.uid.ToString();
-                row[2] = json.charge.ToString();
+                row[2] = json.fid.ToString() + json.charge.ToString();
                 row[3] = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
                 row[4] = result;
                 ret = result;
