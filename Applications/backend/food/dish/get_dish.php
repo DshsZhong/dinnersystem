f09580c2d51b5eeed0c822754c67aef4b93340f5
Sean <seanpai96@gmail.com>
2019-09-05 21:37:11 +0800

    Revert "fk u"
    This reverts commit 5a7b2a3549b2c5eeaf666ff2bae46cfe8a96f7f5.

--- Applications/backend/food/dish/get_dish.php
+++ Applications/backend/food/dish/get_dish.php
@@ -5,7 +5,7 @@ function get_dish($did = null)
 {
     $mysqli = $_SESSION['sql_server'];
 
-    $sql = "SELECT D.id ,D.dish_name ,D.charge ,D.is_idle ,DP.id ,D.is_vegetarian ,IF(F.daily_limit - F.sum >= 0 ,D.daily_limit  ,0)
+    $sql = "SELECT D.id ,D.dish_name ,D.charge ,D.is_idle ,DP.id ,D.is_vegetarian ,IF(F.daily_limit - F.sum = 0 , 0 ,D.daily_limit)
         FROM `dinnersys`.`dish` AS D ,`dinnersys`.`department` AS DP ,`dinnersys`.`factory` AS F
         WHERE D.department_id = DP.id 
 	AND F.id = DP.factory
